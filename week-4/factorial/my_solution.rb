1db7e975d5f499c45bace7c8f36592f50334a5f3
Eazy Doesnt <Candied_Island@Eazys-MBP.attlocal.net>
2015-11-17 10:08:35 -0600

    finishing

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,17 @@
+# Factorial
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def factorial(number)
+  # Your code goes here
+  multiplier = number...1
+  if number <= 1
+    1
+  else
+    p number*(multiplier)
+  end
+end
+
+factorial(3)
