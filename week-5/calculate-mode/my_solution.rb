584102eb572f30a1ebe04355b5414a4694a198c5
Eazy Doesnt <Candied_Island@Eazys-MacBook-Pro.local>
2015-11-30 09:56:18 -0600

    workingOnMode

---
+++ week-5/calculate-mode/my_solution.rb
@@ -0,0 +1,41 @@
+# Calculate the mode Pairing Challenge
+
+# I worked on this challenge [by myself]
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented.
+
+
+
+# 0. Pseudocode
+
+# What is the input? An array of integers or strings
+# What is the output? (i.e. What should the code return?) The code should return the string or integer that repeats the most from the given array.
+# What are the steps needed to solve the problem?
+  #The array needs to be SPLIT into EACH individual piece.
+  #The program needs to COUNT each string or integer
+  #IF there is one integer/string that has a COUNT higher than the rest RETURN/Print to the console that number
+  #Elsif they're all equal print all of the numbers
+
+
+# 1. Initial Solution
+def mode(array)
+  array.sort
+  elements = Hash.new(0)
+
+  array.each do |number|
+    array.count(number)
+  end
+end
+p mode([1,2,3,4,4,3,2])
+
+
+# 3. Refactored Solution
+
+
+
+
+# 4. Reflection
---
+++ week-5/test.rb
@@ -0,0 +1,7 @@
+x = [1,2,3]
+
+p x.object_id
+w = x.dup
+
+p x.object_id
+p w.object_id
