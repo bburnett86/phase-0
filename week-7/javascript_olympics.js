a03a04a0ab176b0a658dd64471ad36c646caf5ef
Eazy Doesnt <Candied_Island@Eazys-MacBook-Pro.local>
2016-01-04 01:24:18 -0600

    7.6 with Chris Wong

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -1,88 +1,109 @@
  // JavaScript Olympics
 
-// I paired [by myself, with:] on this challenge.
+// I paired [with: Chris Wong] on this challenge.
 
-// This challenge took me [#] hours.
+// This challenge took me [2] hours.
 
 
 // Warm Up
-
-
 // Bulk Up
-
+  // Pseudocode:
+    //Input: An array of athletes
+    //Output: String saying an athlete won an event.
+    //Create a function: include name, event, and win properties
+      //The win property prints out the outpur
 // function win(athlete, event){
-//   return athlete + " won the " + event
+//   return athlete + " won the " + event;
 // }
 
-// console.log(win("Michael Phelps", "Breast Stroke"))
+// console.log(win("Hussain Bolt", "100 Yard Dash"))
 
 // Jumble your words
+// Pseudocode:
+// Input: A string
+// Output: string reversed as a string
+// Steps:
+// Create a function that accepts a string
+// split the string
+// store the values inside an array variable
+// take the array and reverse it
+// store the reverse array in the original array variable
+// join the values of the array and place it back into the string variable
+// return the string variable
 
-// function reverseString(string){
-//   array = string.split("");
-//   array = array.reverse();
-//   string = array.join("");
-//   return string;
+// function reverseString (string){
+//   var ary = string.split("");
+//    ary = ary.reverse();
+//   ary = ary.join('');
+//   return ary
 // }
 
-// console.log(reverseString("!sdrow ruoy elbmuJ"))
+// console.log(reverseString("This is a String"));
+
+
+
+
+
 
 // 2,4,6,8
 
-// var array = [2,5,7,3,4,8,54,45,1112];
-// var text = "";
-// i = 0;
-// counter = 0
-// function even(array){
-//   for (i = 0; i <= array.length; i++) {
-//     if (array[i] %2 == 0);
-//       text += array[i];
-//   }
-// }
-// console.log(even(array))
+//Pseudocode:
+  //Input: An array of numbers
+  //Output: An Array of ONLY even numbers
+    //Create a function that determines if the number is even or not
+    //That function must filter the array so only the even numbers remain.
+      //Create an if statement stating if the number is divisible by 2 with no remainder it's even
+      //Else that number is odd
+      //Function must filter argument array
 
-// function even(array){
-//   return value % 2 == 0;
-// }
-// var filtered = [2,5,7,3,4,8,54,45,1112].filter(even);
-
-// // Define a callback function.
-// function checkIfEven(value) {
-//     while(numbers[value]) {
-//         if (value % 2 == 0) {
-//             return false;
-//         }
+// function filter(array){
+//   for (var counter=0; counter < array.length; counter++){
+//     var element = array.indexOf(counter);
+//     if (element % 2 == 0){
+//       var even = element.push;
+//     }else{
+//       var odd = element.push;
 //     }
-//     return true;
+//   return even
+// }
 // }
 
-// // Create the original array.
-// var numbers = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53];
-
-// // Get the prime numbers that are in the original array.
-// var evens = numbers.filter(checkIfEven);
-
-// console.log(evens);
-
-// document.write(evens);
-
-// "We built this city"
-
-var Athlete = function(name, age, sport, quote){
-  this.name = name;
-  this.age = age;
-  this.sport = sport;
-  this. quote = quote;
+function test(number){
+  if (number % 2 == 0)
+   return true
+    else
+   return false
 }
 
+var ary = [1,2,3,4,5,6];
+console.log(ary.filter(test))
+
+// "We built this city"
+//Pseudocode:
+  //Input: Name, Age, Sport, Quote
+  //Output: String
+    // Create a constructor function that will make driver code workcode work
+      //Create a function called athlete that accepts the inputs
+      //Add properties that correspond to the inputs
+
+function Athlete(name, age, sport, quote){
+  this.name=name;
+  this.age=age;
+  this.sport=sport;
+  this.quote=quote;
+}
+
+// driver code
+
 var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+
 console.log(michaelPhelps.constructor === Athlete)
 console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 // Reflection
 
 // What JavaScript knowledge did you solidify in this challenge?
-  // I really solidified Object Oriented JavaScript. Before this challenge I was a bit shaky on what things were called, how certain things related to each other and so forth. However, after having the ability to see that literal notation and construction notation were essentially the JavaScript versions of Instance and Local Methods/Variables, it made it a lot easier to see where things fit in the scheme of things. Also recieving definitions for properties/methods in reation to variables/functions made those things a lot easier to understand as well.
+  // I really solidified Object Oriented JavaScript. Before this challenge I was a bit shaky on what things were called, how certain things related to each other and so forth. However, after having the ability to see that literal notation and construction notation were essentially the JavaScript versions of Instance and Local Methods/Variables, it made it a lot easier to see where things fit in the scheme of things. Also recieving definitions for properties/methods in reation to variables/functions made those things a lot easier to understand as well. Also something I learned once I revisited this with a classmate is how simple things can be if you approach things from a different perspective. By myself I was determined to turn the third problem into a collaboration of a for loop and if control flow statement. When all we really had to do was filter, and it became a much simpler problem.
 // What are constructor functions?
   // Constructor functions are JavaScript's version of Instance Methods where it's a function that can be used/changed throughout the lifetime of the script. Also, this is the preferred notation if you need to do some initial work before the object is created.
 // How are constructors different from Ruby classes (in your research)?
